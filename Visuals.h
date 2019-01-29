707b8e5e42f24d870c8912f6c114eff0f85cb970
Kyle Gough <kylegough98@gmail.com>
2019-01-29 20:50:02 +0000

    Created visual header file.

---
+++ Visuals.h
@@ -0,0 +1,60 @@
+#ifndef VISUALS_H
+#define VISUALS_H
+
+//Light Struct.
+struct Light {
+	size_t name;
+	float ambient[4];
+	float diffuse[4];
+	float specular[4];
+	float position[4];
+};
+
+//Material Struct.
+struct Material {
+	float ambient[4];
+	float diffuse[4];
+	float specular[4];
+	float shininess;
+};
+
+//Global Light Source.
+const Light globalLight = {
+	GL_LIGHT0,
+	{0.3f, 0.3f, 0.3f, 1.0f},
+	{2.0f, 2.0f, 2.0f, 1.0f},
+	{0.5f, 0.5f, 0.5f, 1.0f},
+	{120.0f, 90.0f, 50.0f, 1.0f}
+};
+
+//Global Material.
+const Material globalMaterial = {
+	{0.02f, 0.02f, 0.02f, 1.0f},
+  {0.01f, 0.01f, 0.01f, 1.0f},
+	{0.4f, 0.4f, 0.4f, 1.0f},
+	0.078125f
+
+	/*###{0.15f, 0.15f, 0.15f, 1.0f},
+  {0.4f, 0.4f, 0.4f, 1.0f},
+	{0.7746f, 0.7746f, 0.7746f, 1.0f},
+	76.8f*/
+};
+
+//Sets the properties of a given material.
+void setMaterial(const Material& material) {
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material.ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material.diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, material.specular);
+	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material.shininess);
+}
+
+//Sets the properties of a given light.
+void setLight(const Light& light) {
+	glLightfv(light.name, GL_AMBIENT, light.ambient);
+	glLightfv(light.name, GL_DIFFUSE, light.diffuse);
+	glLightfv(light.name, GL_SPECULAR, light.specular);
+	glLightfv(light.name, GL_POSITION, light.position);
+	//glLightf(light.name, GL_SPOT_CUTOFF, 60.0f);
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -14,66 +14,9 @@
 #include "SimplexNoise.h" //Perlin Noise.
 #include "Draw.h" //Draw functions.
 #include "Cell.h" //Cell struct.
+#include "Visuals.h" //Lighting and Materials.
 #include "Drone.h" //Drone object and functions.
 using namespace std;
-
-/*@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@*/
-
-//Light Struct.
-struct Light {
-	size_t name;
-	float ambient[4];
-	float diffuse[4];
-	float specular[4];
-	float position[4];
-};
-
-//Material Struct.
-struct Material {
-	float ambient[4];
-	float diffuse[4];
-	float specular[4];
-	float shininess;
-};
-
-//Global Light Source.
-const Light globalLight = {
-	GL_LIGHT0,
-	{0.3f, 0.3f, 0.3f, 1.0f},
-	{2.0f, 2.0f, 2.0f, 1.0f},
-	{0.5f, 0.5f, 0.5f, 1.0f},
-	{120.0f, 90.0f, 50.0f, 1.0f}
-};
-
-//Global Material.
-const Material globalMaterial = {
-	{0.02f, 0.02f, 0.02f, 1.0f},
-  {0.01f, 0.01f, 0.01f, 1.0f},
-	{0.4f, 0.4f, 0.4f, 1.0f},
-	0.078125f
-
-	/*###{0.15f, 0.15f, 0.15f, 1.0f},
-  {0.4f, 0.4f, 0.4f, 1.0f},
-	{0.7746f, 0.7746f, 0.7746f, 1.0f},
-	76.8f*/
-};
-
-//Sets the properties of a given material.
-void setMaterial(const Material& material) {
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material.ambient);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material.diffuse);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, material.specular);
-	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material.shininess);
-}
-
-//Sets the properties of a given light.
-void setLight(const Light& light) {
-	glLightfv(light.name, GL_AMBIENT, light.ambient);
-	glLightfv(light.name, GL_DIFFUSE, light.diffuse);
-	glLightfv(light.name, GL_SPECULAR, light.specular);
-	glLightfv(light.name, GL_POSITION, light.position);
-	//glLightf(light.name, GL_SPOT_CUTOFF, 60.0f);
-}
 
 /*@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@*/
 
@@ -179,7 +122,6 @@ void randomiseCave() {
 		}
 	}
 }
-
 
 /*@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@~#~@*/
 
